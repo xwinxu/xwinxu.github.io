6bd10d69fa345dd60320cb44de14af728f6523a9
Winnie Xu <winnie.xu97@gmail.com>
2019-10-22 11:01:47 -0400

    change speed

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
     var atTop = document.getElementById('scrollable');
-    // if (prevScrollpos > currentScrollPos) {
-    //     document.getElementById("navbar").style.top = "0";
-    // } else {
-    //     document.getElementById("navbar").style.top = "-50px";
-    // }
     if (prevScrollpos < currentScrollPos) {
         document.getElementById("navbar").style.top = "-50px";
     } else {
@@ -26,8 +21,7 @@ document.querySelector("#researchBtn").addEventListener("click", function() {
     document.querySelector("#research").classList.toggle('hidden');
     document.querySelector("#projects").classList.add('hidden');
     document.querySelector("#projectBtn").classList.remove('highlight');
-    pJSDom[0].pJS.particles.move.speed = 2;
-
+    pJSDom[0].pJS.particles.move.speed = 5;
 })
 
 document.querySelector("#projectBtn").addEventListener("click", function() {
@@ -35,5 +29,5 @@ document.querySelector("#projectBtn").addEventListener("click", function() {
     document.querySelector("#projects").classList.toggle('hidden');
     document.querySelector("#research").classList.add('hidden');
     document.querySelector("#researchBtn").classList.remove('highlight');
-    pJSDom[0].pJS.particles.move.speed = 2;
+    pJSDom[0].pJS.particles.move.speed = 5;
 })
