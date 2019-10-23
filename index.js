6171cfa03a2efb93f36e150c5c3cdd77bb64e0a2
Winnie Xu <winnie.xu97@gmail.com>
2019-10-22 23:25:19 -0400

    fix error and add dynamic

--- index.js
+++ index.js
@@ -16,18 +16,44 @@ window.onscroll = function () {
     prevScrollpos = currentScrollPos;
 }
 
+let isResearchHidden = true;
 document.querySelector("#researchBtn").addEventListener("click", function() {
     this.classList.toggle('highlight');
+
+
     document.querySelector("#research").classList.toggle('hidden');
     document.querySelector("#projects").classList.add('hidden');
+    isProjectHidden = true;
+
     document.querySelector("#projectBtn").classList.remove('highlight');
+
+    if (isResearchHidden) {
+    pJSDom[0].pJS.particles.move.speed = 2;
+
+    } else {
+
     pJSDom[0].pJS.particles.move.speed = 5;
+    }
+
+    isResearchHidden = !isResearchHidden;
 })
 
+let isProjectHidden = true;
 document.querySelector("#projectBtn").addEventListener("click", function() {
     this.classList.toggle('highlight');
     document.querySelector("#projects").classList.toggle('hidden');
     document.querySelector("#research").classList.add('hidden');
     document.querySelector("#researchBtn").classList.remove('highlight');
+    isResearchHidden = true;
+    pJSDom[0].pJS.particles.move.speed = 2;
+
+    if (isProjectHidden) {
+    pJSDom[0].pJS.particles.move.speed = 2;
+
+    } else {
+
     pJSDom[0].pJS.particles.move.speed = 5;
+    }
+
+    isProjectHidden = !isProjectHidden;
 })
